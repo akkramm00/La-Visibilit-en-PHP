8a641b214d7030bdfa0aab17dafb0f6f1f996b5c
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-31 18:23:26 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+
+    <h2> La visibilité en PHP</h2>
+<p>
+  La visibilité en PHP permetde définir les niveaux d'accès aux propiriétés et aux méthodes d'une classe. Il exixte trois niveaux de visibilité en PHP:
+  PUBLIC : permet à la propriété ou à la méthoide d'etre accessible depuis n'importe ou dans le code.
+  PROTECTED : permet à la propriété ou lma méthode d'etre accessible dans la classe elle-meme et dans les classes dérivées (classes enfants ou héritières).
+  PRIVATE: limite l'accès à la propriété ou à la méthode à la classe elle-meme.
+    <?php
+
+
+?> 
+
+  </body>
+</html>
